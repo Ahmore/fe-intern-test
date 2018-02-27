7ac24d478a035b40710948b925b7e5184ebd7c3e
Dariusz Szymczyk <edarek@op.pl>
2018-02-27 13:39:44 +0100

    Scroller perfomance improvement.

--- js/scroller.js
+++ js/scroller.js
@@ -1,9 +1,19 @@
 (function (app) {
     function Scroller() {
+        this.timer;
+
         $(window).scroll(this.onScroll.bind(this));
     }
 
     Scroller.prototype.onScroll = function() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+        }
+
+        this.timer = setTimeout(this.handleScroll.bind(this), app.config.SCROLLER_DELAY);
+    };
+
+    Scroller.prototype.handleScroll = function() {
         var scrollHeight = $(document).height();
         var scrollPosition = $(window).height() + $(window).scrollTop();
 
